f18da8bdb69b3bf558dad161d467df356581a057
Alejandro Marcano <+>
2021-11-23 04:24:27 +0100

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
+  get '/students/:id', to: 'students#id'
   get '/students/highest-grade', to: 'students#highest_grade'
 end
